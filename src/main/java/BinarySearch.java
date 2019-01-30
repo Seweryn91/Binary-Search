99b536757ca0dd1bbca467c5bdbefc9ee8bf38e8
Seweryn91 <s.talaj@gmail.com>
2019-01-30 17:40:54 +0100

    Implement method search

--- src/main/java/BinarySearch.java
+++ src/main/java/BinarySearch.java
@@ -9,4 +9,30 @@ public class BinarySearch {
         Collections.sort(inputList);
         return inputList;
     }
+
+    public boolean search(List<Integer> inputList, Integer toFind) {
+        List<Integer> sortedList = sortList(inputList);
+        int upperBound = sortedList.size();
+        int lowerBound = 1;
+
+        for (int i = 0; i < sortedList.size(); i++) { // Using "while" loop with toFind not
+                                                      // present && lowerBound < toFind < upperBound
+                                                      // creates an infinite loop.
+            if (toFind < lowerBound) {
+                return false;
+            }
+
+            int midpoint = lowerBound + (upperBound - lowerBound) / 2;
+
+            if (sortedList.get(midpoint) < toFind) {
+                lowerBound = midpoint + 1;
+            } else if (sortedList.get(midpoint) > toFind) {
+                upperBound = midpoint - 1;
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+
 }
