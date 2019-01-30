7d0b8b84da61c9839d80f7971d8742c1787a2b36
Seweryn91 <s.talaj@gmail.com>
2019-01-30 17:39:19 +0100

    Implement method sortList

--- src/main/java/BinarySearch.java
+++ src/main/java/BinarySearch.java
@@ -1,3 +1,12 @@
+import java.util.Collections;
+import java.util.List;
+
 public class BinarySearch {
 
+    public BinarySearch() {}
+
+    private List<Integer> sortList(List<Integer> inputList) {
+        Collections.sort(inputList);
+        return inputList;
+    }
 }
